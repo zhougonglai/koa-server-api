c2067899936fcfa231a3a659c0d98cd70c10a29c
john.zhou <john.zhou@abc360.com>
2019-04-22 08:35:50 +0800

    type: update

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,15 @@ const Mock = require('mockjs');
 const router = new Router();
 
 router
+  .get('home', '/' , async (ctx, next) => {
+    ctx.body = {
+      state: true,
+      message: 'sucess',
+      data: "success",
+      code: 200
+    }
+    await next();
+  })
   .get('/lists', async (ctx, next) => {
       ctx.body = {
         ...ctx.body,
